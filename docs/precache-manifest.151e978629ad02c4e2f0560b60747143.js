bc635bb4abf2677cdbb128dd9bd4cf771c80dab1
DiegoTKD94 <diego.chavarriap@gmail.com>
2020-05-11 22:51:34 -0500

    Todo ok con correcciones, carpeta docs añadida

---
+++ docs/precache-manifest.151e978629ad02c4e2f0560b60747143.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d6fc08a8eeab93125f27db114112001",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58f4786620571677d4ea",
+    "url": "/static/css/2.c3343ac3.chunk.css"
+  },
+  {
+    "revision": "1d470e91b197c83d0041",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "58f4786620571677d4ea",
+    "url": "/static/js/2.d28e4d2d.chunk.js"
+  },
+  {
+    "revision": "2efc0b494659ead92be8e25c4065b97c",
+    "url": "/static/js/2.d28e4d2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d470e91b197c83d0041",
+    "url": "/static/js/main.dbecb937.chunk.js"
+  },
+  {
+    "revision": "fcc54b90dd4d35c0f31a",
+    "url": "/static/js/runtime-main.92837332.js"
+  }
+]);
